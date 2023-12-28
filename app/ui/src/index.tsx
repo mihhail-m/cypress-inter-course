9ff62a84c7624f6039b69efdc9b06b53a4bcc1bd
Michanix <michanix@protonmail.com>
2023-12-28 18:15:17 +0200

    added apollo client for react

--- app/ui/src/index.tsx
+++ app/ui/src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import './index.css';
 import App from './App';
+
+const gqlUri = process.env.GRAPHQL_URL;
+const client = new ApolloClient({
+  uri: gqlUri,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
